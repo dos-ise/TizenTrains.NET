a7e66fdb56a26b941136fe3909dce2d4f5189808
Dennis <ostermann3k@googlemail.com>
2026-08-13 07:59:42 +0200

    Added TizenTrains Project

---
+++ src/TizenTrains/src/Main.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+
+namespace TizenTrains
+{
+    class TizenTrainsDemo
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            //Tizen.Log.Debug("NUI", "Main() called");
+            TrainsApp trainsApp = new TrainsApp();
+
+            float version = 0.1f;
+            Tizen.Log.Info("TizenTrains", "Trains.NET NUI TV-port. Version: " + version);
+
+            // Process command line arguments.
+            if (args.Length > 0)
+            {
+                string argumentList = "";
+                for (int i = 0; i < args.Length; ++i)
+                {
+                    argumentList += args[i] + " ";
+                }
+
+                Tizen.Log.Info("TizenTrains", "Argument list: " + argumentList);
+            }
+
+            trainsApp.Run(args);
+            return 0;
+        }
+    }
+}
---
+++ src/TizenTrains/src/TrainsApp.cs
@@ -0,0 +1,74 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using SkiaSharp;
+using Trains.NET.Rendering;
+using Trains.NET.Rendering.Skia;
+using Timer = Tizen.NUI.Timer;
+
+namespace TizenTrains
+{
+    public class TrainsApp : NUIApplication
+    {
+        private Window _window;
+        private ImageView _imageView;
+        private IGame _game;
+        private IInteractionManager _interactionManager;
+        private SKBitmap _bitmap;
+        private SKCanvasWrapper _canvasWrapper;
+        private int _width;
+        private int _height;
+
+        public TrainsApp() : base("", WindowMode.Opaque) { }
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            OnInitialize();
+        }
+
+        private void OnInitialize()
+        {
+            _window = Window.Default;
+            _window.SetOpaqueState(true);
+
+            _width = _window.Size.Width;
+            _height = _window.Size.Height;
+
+            _game = DI.ServiceLocator.GetService<IGame>();
+            _interactionManager = DI.ServiceLocator.GetService<IInteractionManager>();
+
+            _bitmap = new SKBitmap(_width, _height);
+            using (SKCanvas canvas = new SKCanvas(_bitmap))
+            {
+                _canvasWrapper = new SKCanvasWrapper(canvas);
+            }
+
+            _imageView = new ImageView
+            {
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.FillToParent
+            };
+            _window.Add(_imageView);
+
+            _game.InitializeAsync(200, 200).GetAwaiter().GetResult();
+            _game.SetSize(_width, _height);
+
+            _window.KeyEvent += OnKeyEvent;
+
+            Timer renderTimer = new Timer(33);
+            renderTimer.Tick += OnRenderTick;
+            renderTimer.Start();
+        }
+
+        private bool OnRenderTick(object source, Timer.TickEventArgs e)
+        {
+            return true;
+        }
+
+
+        private void OnKeyEvent(object source, Window.KeyEventArgs e)
+        {
+
+        }
+    }
+}
